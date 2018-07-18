92b21c9cb44292831d79cde7677133b5d7ea075d
Christopher Henn <chris@chrishenn.net>
2018-07-18 09:34:43 -0700

    Add empty state to AnnotationControlBar

--- ui/src/shared/components/AnnotationControlBar.tsx
+++ ui/src/shared/components/AnnotationControlBar.tsx
@@ -4,13 +4,24 @@ class AnnotationControlBar extends PureComponent {
   public render() {
     return (
       <div className="annotation-control-bar">
-        <div className="annotation-control-bar--lhs" />
+        <div className="annotation-control-bar--lhs">
+          {this.renderEmptyState()}
+        </div>
         <div className="annotation-control-bar--rhs">
           <div className="btn btn-primary btn-sm">
             <span className="icon plus" />
             Add Annotation
           </div>
         </div>
+      </div>
+    )
+  }
+
+  private renderEmptyState(): JSX.Element {
+    return (
+      <div className="annotation-control-bar--empty">
+        There are no <strong>Annotation Labels</strong> present in this time
+        range
       </div>
     )
   }
